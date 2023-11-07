602d1c8f9bfefb28f7ec5bea6092599b6ce499af
Arthur Kim <artur07kim.com@gmail.com>
2023-11-07 11:58:08 +0200

    Implemented ISunriseSunsetService

---
+++ SunriseSunsetTracker.Api/Interfaces/ISunriseSunsetService.cs
@@ -0,0 +1,10 @@
+﻿using SunriseSunsetTracker.Api.Data.DTO;
+
+namespace SunriseSunsetTracker.Api.Interfaces;
+
+public interface ISunriseSunsetService
+{
+    public Task<GetSunriseSunsetResponse> GetSunriseSunsetTime(
+        float latitude,
+        float longitude);
+}
---
+++ SunriseSunsetTracker.Api/Services/SunriseSunsetService.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using SunriseSunsetTracker.Api.Data.DTO;
+using SunriseSunsetTracker.Api.Interfaces;
+using SunriseSunsetTracker.Api.Utils.Extensions;
+
+namespace SunriseSunsetTracker.Api.Services;
+
+public class SunriseSunsetService : ISunriseSunsetService
+{
+    private readonly HttpClient _httpClient;
+
+    public SunriseSunsetService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<GetSunriseSunsetResponse> GetSunriseSunsetTime(
+        float latitude,
+        float longitude)
+    {
+        var values = new Dictionary<string, string>
+        {
+            { nameof(latitude), latitude.ToString(CultureInfo.InvariantCulture) },
+            { nameof(longitude), longitude.ToString(CultureInfo.InvariantCulture) }
+        };
+
+        var requestContent = await new FormUrlEncodedContent(values).ReadAsStringAsync();
+        var response =  await _httpClient.GetAsync($"?{requestContent}");
+
+        var result = await response.DeserializeResponseAsync<GetSunriseSunsetResponse>();
+
+        return result;
+    }
+}
