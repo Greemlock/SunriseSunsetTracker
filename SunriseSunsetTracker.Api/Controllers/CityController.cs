ebc8f23a5f25bb52e5ce38bb8cc4016d83c5cd9d
Arthur Kim <artur07kim.com@gmail.com>
2023-11-07 11:58:24 +0200

    Added base controllers

---
+++ SunriseSunsetTracker.Api/Controllers/CityController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunriseSunsetTracker.Api.Data.Entities;
+
+namespace SunriseSunsetTracker.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CityController : ControllerBase
+{
+    private readonly ILogger<CityController> _logger;
+
+    public CityController(ILogger<CityController> logger)
+    {
+        _logger = logger;
+    }
+
+    // [HttpGet("{id}")]
+    // public async Task<City> GetCity(ulong id) => 
+    //     await Task.FromResult(new GetCityResponseModel
+    //     {
+    //         City = new City()
+    //         {
+    //             Id = 1,
+    //             Name = "WeatherForecast",
+    //             GeoCoordinates = (1, 1)
+    //         }
+    //     });
+    //
+    // [HttpGet]
+    // public async Task<GetAllCitiesResponseModel> GetAllCities() => 
+    //     await Task.FromResult(new GetAllCitiesResponseModel
+    //     {
+    //         Cities = new List<City>()
+    //         {
+    //             new City()
+    //             {
+    //                 Id = 1,
+    //                 Name = "WeatherForecast",
+    //                 GeoCoordinates = (1, 1)
+    //             },
+    //             new City()
+    //             {
+    //                 Id = 2,
+    //                 Name = "WeatherForecast",
+    //                 GeoCoordinates = (2, 2)
+    //             }
+    //         },
+    //     });
+    //
+    // [HttpPost]
+    // public async Task<IActionResult> AddCity(AddCityRequest request) => await Task.FromResult(Created(request.City.Id.ToString(), request.City));
+    //
+    // [HttpPost]
+    // public async Task<IActionResult> AddMultipleCities(AddMultipleCitiesRequest request) => await Task.FromResult(Created());
+}
---
+++ SunriseSunsetTracker.Api/Controllers/EventTimeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunriseSunsetTracker.Api.Interfaces;
+
+namespace SunriseSunsetTracker.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EventTimeController : ControllerBase
+{
+    private readonly ISunriseSunsetService _sunriseSunsetService;
+
+    public EventTimeController(ISunriseSunsetService sunriseSunsetService)
+    {
+        _sunriseSunsetService = sunriseSunsetService;
+    }
+    
+    [HttpGet("latitude={latitude}&longitude={longitude}")]
+    public async Task<IActionResult> GetTodaySunriseSunsetTime(float latitude, float longitude)
+    {
+        var response = await _sunriseSunsetService.GetSunriseSunsetTime(latitude, longitude);
+        
+        return Ok(new
+        {
+            response.Results.Sunrise,
+            response.Results.Sunset
+        });
+    }
+}
