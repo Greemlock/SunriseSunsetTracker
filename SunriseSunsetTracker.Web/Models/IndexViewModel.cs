d29701edfdfb9fa47c68e7f18ed93f9bb7e65eee
Arthur Kim <artur07kim.com@gmail.com>
2023-11-08 14:27:56 +0200

    Implemented base web view

---
+++ SunriseSunsetTracker.Web/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SunriseSunsetTracker.Web.Models;
+
+public class IndexViewModel
+{
+    public List<Card> Cards { get; init; } = new List<Card>();
+}
+
+public class Card
+{
+    public required string Name { get; set; }
+    public required (double Latitude, double Longitude) GeoCoordinates { get; set; }
+}
