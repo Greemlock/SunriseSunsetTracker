2b719a1a991a49ee82bbfd6079cc1a9356ba9889
Arthur Kim <artur07kim.com@gmail.com>
2023-11-10 10:19:14 +0200

    Added tests for methods event time controller

--- SunsetSunriseTracker.Api.Tests/Controllers/EventTimeControllerTestFixture.cs
+++ SunsetSunriseTracker.Api.Tests/Controllers/EventTimeControllerTestFixture.cs
@@ -2,7 +2,6 @@
 using SunriseSunsetTracker.Api.Controllers;
 using SunriseSunsetTracker.Api.Data.DTO;
 using SunriseSunsetTracker.Api.Interfaces;
-using SunriseSunsetTracker.Api.Services;
 using SunriseSunsetTracker.Common.Contracts.Responses;
 
 namespace SunsetSunriseTracker.Api.Tests.Controllers;
@@ -19,47 +18,136 @@ public class EventTimeControllerTestFixture
         _eventTimeController = new EventTimeController(_sunriseSunsetServiceMock.Object);
 
     }
-    
+
     [Test]
-    public async Task GetTodaySunriseSunsetTime_ValidCoordinates_ReturnsOkWithEventTimeResponse()
+    public async Task GetTodaySunriseSunsetTime_WithValidCoordinates_ReturnsOkResult()
     {
         // Arrange
-        const double validLatitude = 37.7749;
-        const double validLongitude = -122.4194;
-        var expectedSunriseTime = DateTime.Now;
-        var expectedSunsetTime = DateTime.Now;
-
-        var sunriseSunsetResponse = new GetSunriseSunsetResponse
-        {
-            Results = new SunriseSunsetInfo
+        const double latitude = 12.34;
+        const double longitude = 56.78;
+        _sunriseSunsetServiceMock.Setup(x => x.GetSunriseSunsetTimeAsync(
+            It.IsAny<double>(),
+            It.IsAny<double>()))
+            .ReturnsAsync(new GetSunriseSunsetResponse
             {
-                Sunrise = expectedSunriseTime,
-                Sunset = expectedSunsetTime
-            }
-        };
+                
+                Results = new SunriseSunsetInfo
+                {
+                    Sunrise = DateTime.Now.AddHours(6),
+                    Sunset = DateTime.Now.AddHours(18)
+                }
+            });
+        
+        // Act
+        var result = await _eventTimeController.GetTodaySunriseSunsetTime(latitude, longitude);
 
-        _sunriseSunsetServiceMock.Setup(service =>
-            service.GetSunriseSunsetTimeAsync(validLatitude, validLongitude)).ReturnsAsync(sunriseSunsetResponse);
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = (OkObjectResult)result;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That(okResult.Value, Is.InstanceOf<SunriseSunsetTime>());
+    }
+    
+    [Test]
+    public async Task GetTodaySunriseSunsetTime_WithInvalidCoordinates_ReturnsBadRequest()
+    {
+        // Arrange
+        const int invalidLatitude = 100;
+        const int invalidLongitude = 200;
 
         // Act
-        var result = await _eventTimeController.GetTodaySunriseSunsetTime(validLatitude, validLongitude);
+        var result = await _eventTimeController.GetTodaySunriseSunsetTime(invalidLatitude, invalidLongitude);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
+    }
+    
+    [Test]
+    public async Task GetTodaySunriseTime_ValidCoordinates_ReturnsOkWithSunriseTime()
+    {
+        // Arrange
+        const double latitude = 40.7128;
+        const double longitude = -74.0060;
+        
+        _sunriseSunsetServiceMock.Setup(x => x.GetSunriseSunsetTimeAsync(
+                It.IsAny<double>(),
+                It.IsAny<double>()))
+            .ReturnsAsync(new GetSunriseSunsetResponse
+            {
+                
+                Results = new SunriseSunsetInfo
+                {
+                    Sunrise = DateTime.Now.AddHours(6),
+                    Sunset = DateTime.Now.AddHours(18)
+                }
+            });
+
+        // Act
+        var result = await _eventTimeController.GetTodaySunriseTime(latitude, longitude);
 
         // Assert
         Assert.That(result, Is.InstanceOf<OkObjectResult>());
         var okResult = result as OkObjectResult;
         Assert.That(okResult, Is.Not.Null);
-        Assert.Multiple(() =>
-        {
-            Assert.That(okResult!.Value, Is.Not.Null);
-            Assert.That(okResult.Value, Is.InstanceOf<EventTimeResponseModel>());
-        });
-        
-        var eventTimeResponse = okResult!.Value as EventTimeResponseModel;
-        Assert.That(eventTimeResponse, Is.Not.Null);
-        Assert.Multiple(() =>
-        {
-            Assert.That(eventTimeResponse!.SunriseTime, Is.EqualTo(expectedSunriseTime));
-            Assert.That(eventTimeResponse.SunsetTime, Is.EqualTo(expectedSunsetTime));
-        });
+        Assert.That(okResult!.Value, Is.InstanceOf<DateTime>());
     }
+
+    [Test]
+    public async Task GetTodaySunriseTime_InvalidCoordinates_ReturnsBadRequest()
+    {
+        // Arrange
+        const double invalidLatitude = 100.0;
+        const double invalidLongitude = -74.0060;
+
+        // Act
+        var result = await _eventTimeController.GetTodaySunriseTime(invalidLatitude, invalidLongitude);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
+    }
+    
+    [Test]
+    public async Task GetTodaySunsetTime_ValidCoordinates_ReturnsOkWithSunriseTime()
+    {
+        // Arrange
+        const double latitude = 40.7128;
+        const double longitude = -74.0060;
+        
+        _sunriseSunsetServiceMock.Setup(x => x.GetSunriseSunsetTimeAsync(
+                It.IsAny<double>(),
+                It.IsAny<double>()))
+            .ReturnsAsync(new GetSunriseSunsetResponse
+            {
+                
+                Results = new SunriseSunsetInfo
+                {
+                    Sunrise = DateTime.Now.AddHours(6),
+                    Sunset = DateTime.Now.AddHours(18)
+                }
+            });
+
+        // Act
+        var result = await _eventTimeController.GetTodaySunsetTime(latitude, longitude);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That(okResult!.Value, Is.InstanceOf<DateTime>());
+    }
+
+    [Test]
+    public async Task GetTodaySunsetTime_InvalidCoordinates_ReturnsBadRequest()
+    {
+        // Arrange
+        const double invalidLatitude = 100.0;
+        const double invalidLongitude = -74.0060;
+
+        // Act
+        var result = await _eventTimeController.GetTodaySunsetTime(invalidLatitude, invalidLongitude);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
+    }
+
 }
